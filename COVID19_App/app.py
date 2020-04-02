0a8fd052467b3ccb0c4c189e87891765d66528c3
gabrielsaruhashi <gabriel.saruhashi@gmail.com>
2020-04-02 01:44:05 -0400

    first frontend pass

--- COVID19_App/app.py
+++ COVID19_App/app.py
@@ -1,3 +1,5 @@
+
+# coding=utf-8
 import streamlit as st
 
 import plotly.express as px
--- COVID19_App/simulation.py
+++ COVID19_App/simulation.py
@@ -1,23 +1,83 @@
 import streamlit as st
+import enum
+
+class Colors(enum.Enum):
+        RED='red-bg'
+        YELLOW='yellow-bg'
+        ORANGE='orange-bg'
+        GREEN='green-bg'
+
+class Documents(enum.Enum):
+        METHODOLOGY='https://docs.google.com/document/d/1C7LyLmeeQVV0A3vRqH03Ru0ABdJ6hCOcv_lYVMPQy2M/edit'
+        FAQ='https://docs.google.com/document/d/1lanC52PjzU2taQISs1kO9mEJPtvwZM4uyvnHL9IalbQ/edit'
+        GITHUB='https://github.com/ImpulsoGov/simulacovid/tree/master/COVID19_App'
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+
+def generateKPIRow(label1, value1, label2, value2):
+        st.write('''
+        <div class='kpi-wrapper'>
+                <div class='kpi-container green-bg'>
+                        <h3>%s:</h3>   
+                        <span class='kpi'>%s</span>    
+                </div>
+                <div class='kpi-container green-bg'>
+                        <h3>%s:</h3>
+                        <span class='kpi'>%s</span>
+                </div>
+        <div>''' % (label1, value1, label2, value2), 
+        unsafe_allow_html=True)
+
+def generateSimulatorOutput(color, min_range, max_range, label):
+        st.write('''
+                <div class="simulator-output-wrapper %s">
+                        <h3>entre</h3>
+                        <div class="simulator-output-row">
+                                <div class="simulator-output-row-prediction">
+                                        <span class="simulator-output-row-prediction-value">%i</span>
+                                        <span class="simulator-output-row-prediction-separator">e</span>
+                                        <span class="simulator-output-row-prediction-value">%i</span>
+                                </div>
+                                <span class="simulator-output-row-prediction-label">
+                                        *DIAS SERÁ ATINGIDA A CAPACIDADE MÁXIMA DE %s
+                                </span>
+                        </div> 
+                </div>
+        ''' % (color.value, min_range, max_range, label),
+        unsafe_allow_html=True)
 
 def main():
+        local_css("style.css")
+
+
         st.title("SimulaCovid")
         st.subheader('Como seu município pode se preparar para a Covid-19')
         
 
         st.write('## SimulaCovid é um simulador da demanda por leitos hospitalares e ventiladores.')        
 
-        st.write('''
-*Usando dados do DataSUS e de casos confirmados, ele estima por quantos dias - até o limite de 90 - 
+        st.write('''<i>
+Usando dados do DataSUS e de casos confirmados, ele estima por quantos dias - até o limite de 90 - 
 durará o estoque desses equipamentos em cada município. Ao explorar diferentes estratégias de resposta 
 à crise, a ferramenta permite que gestores públicos simulem abordagens e planejem seu curso de ação para 
-evitar o colapso do sistema.*
-        ''')
+evitar o colapso do sistema.</i>
+        ''', unsafe_allow_html=True)
+
 
 
         st.write('''
-Confira nossa metodologia e o github do projeto. Acesse nossas Perguntas Frequentes.
-        ''')
+        <br/>
+        <i>
+                Confira nossa
+                <a href="%s">metodologia</a>
+                e o 
+                <a href="%s">github</a> 
+                do projeto. Acesse nossas 
+                <a href="%s">Perguntas Frequentes.</a>
+        </i>''' % (Documents.METHODOLOGY.value, Documents.GITHUB.value, Documents.FAQ.value), unsafe_allow_html=True)
 
         st.write('## Qual a situação do seu município?')     
 
@@ -25,98 +85,112 @@ Confira nossa metodologia e o github do projeto. Acesse nossas Perguntas Frequen
 
         st.selectbox('Município', ['Campinas'])
 
-
-        st.write('# <CASOS CONFIRMADOS>')
-        st.write('# <MORTES CONFIRMADAS>')
+        generateKPIRow("CASOS CONFIRMADOS", "53.231", "MORTERS CONFIRMADAS", "1343")
 
         st.write('''
 **Fonte:** Brasil.IO atualizado diariamente com base em boletins das secretarias de saúde publicados.
         ''')
 
         st.write('''
-**Lembramos que podem existir casos não diagnosticados em sua cidade.** Sugerimos que consulte o
- Checklist para orientações específicas sobre o que fazer antes do primeiro caso diagnosticado.
-        ''')
+        <div class="info">
+                <span>
+                        <b>Lembramos que podem existir casos não diagnosticados em sua cidade.</b> Sugerimos que consulte o
+                        Checklist para orientações específicas sobre o que fazer antes do primeiro caso diagnosticado.
+                </span>
+        ''', unsafe_allow_html=True)
 
         st.write('''
 ### Seu município tem a seguinte **capacidade hospitalar:**
         ''')
 
-        st.write('# <LEITOS>')
-        st.write('# <VENTILADORES>')
+        generateKPIRow("LEITOS", "53.231", "VENTILADORES", "1343")
 
         st.write('''
-Fonte: DATASUS CNes, Fevereiro 2020. Incluímos leitos hospitalares da rede SUS 
-e não-SUS. Para excluir a última categoria, precisaríamos estimar também a 
-opulação susdependente. Para mais informações, confira nossa metodologia.
-        ''')
+        <b>Fonte:</b> DATASUS CNes, Fevereiro 2020. Incluímos leitos hospitalares da rede SUS 
+        e não-SUS. Para excluir a última categoria, precisaríamos estimar também a 
+        opulação susdependente. Para mais informações, confira nossa
+        <a href="%s" target="blank">metodologia</a>.
+        ''' % (Documents.METHODOLOGY.value), unsafe_allow_html=True)
 
 
         st.write('''
-A maioria das pessoas que contraem Covid-19, conseguem se recuperar em casa - 
-mas uma parte irá desenvolver sintomas graves e precisará de internação em 
-leitos hospitalares. Outros terão sintomas críticos e precisarão de ventiladores 
-mecânicos e tratamento intensivo (UTI). Apesar de serem necessários outros 
-insumos, esses têm sido fatores limitantes na resposta à crise.
-        ''')
+        <div class="info">
+                A maioria das pessoas que contraem Covid-19, conseguem se recuperar em casa - 
+                mas uma parte irá desenvolver sintomas graves e precisará de internação em 
+                leitos hospitalares. Outros terão sintomas críticos e precisarão de ventiladores 
+                mecânicos e tratamento intensivo (UTI). Apesar de serem necessários outros 
+                insumos, esses têm sido fatores limitantes na resposta à crise.
+        <div>
+        ''', unsafe_allow_html=True)
+
+        st.write('<br/>', unsafe_allow_html=True)
 
         st.write('''
-### Assumiremos que 20% destes poderão ser destinados a pacientes com Covid-19 
-(você poderá ajustar abaixo). Caso seu município conte apenas com atitudes 
-individuais, **sem políticas de restrição de contato, estima-se que....**
-        ''')
+        <div class="scenario">
+                <h3>
+                        Assumiremos que 20% destes poderão ser destinados a pacientes com Covid-19 (você poderá ajustar abaixo). 
+                        Caso seu município conte apenas com atitudes sindividuais, **sem políticas de restrição de contato, estima-se que....**
+                </h3>
+        </div>
+        ''', unsafe_allow_html=True)
 
-        st.write('# <DDAY LEITOS WORST>')
-        st.write('# <DDAY VENTILADORES WORST>')
+        generateSimulatorOutput(Colors.RED, 24, 25, 'LEITOS')
+        
+        st.write('<br/>', unsafe_allow_html=True)
+
+        generateSimulatorOutput(Colors.ORANGE, 24, 25, 'VENTILADORES')
+        
 
         st.write('''
-### Caso o município decrete hoje o isolamento social, **fechando comércios e 
-suspendendo transporte público, além de quarentena para doentes, estima-se que...**
-        ''')
+        <div class="scenario">
+                <h3>
+                        Caso o município decrete hoje o isolamento social, <b>e fechando comércios e suspendendo transporte público, além de quarentena para doentes, estima-se que...</b>
+                </h3>
+        </div>
+        ''', unsafe_allow_html=True)
 
-        st.write('# <DDAY LEITOS BEST>')
-        st.write('# <DDAY VENTILADORES BEST>')
+        generateSimulatorOutput(Colors.GREEN, 24, 25, 'LEITOS')
+        
+        st.write('<br/>', unsafe_allow_html=True)
+
+        generateSimulatorOutput(Colors.GREEN, 24, 25, 'VENTILADORES')
 
         st.write('# E como me preparo?')
 
         st.write("""
-Em Wuhan, na China, onde foi registrado o primeiro caso de Covid-19, o governo 
-eagiu em fases para controlar a transmissão da doença, expandir sua capacidade 
-de tratar casos graves e críticos e aliviar a pressão no sistema de saúde pública.
+        Em Wuhan, na China, onde foi registrado o primeiro caso de Covid-19, **o governo reagiu em fases para controlar a transmissão da doença, expandir sua capacidade de tratar casos graves e críticos e aliviar a pressão no sistema de saúde pública.**
 
-Este modelo se baseia na sequência de estratégias apresentadas abaixo (veja Metodologia):        
-""")
+        Este modelo se baseia na sequência de estratégias apresentadas abaixo (veja <a href="%s" target="blank">Metodologia</a>):        
+        """% (Documents.METHODOLOGY), unsafe_allow_html=True)
 
         st.write("""
-**ESTRATÉGIA 1:** Mais pessoas escutam falar da Covid-19, com o início da 
-confirmação de casos. Algumas começam a tomar precauções individuais, mas 
-nenhuma medida de restrição de contato é adotada pelas autoridades.     
-""")
+        <i>**ESTRATÉGIA 1:** Mais pessoas escutam falar da Covid-19, com o início da 
+        confirmação de casos. Algumas começam a tomar precauções individuais, mas 
+        **nenhuma medida de restrição de contato é adotada pelas autoridades.**</i>
+        """, unsafe_allow_html=True)
 
         st.write("""
-**ESTRATÉGIA 2:** O governo decreta o fechamento das fronteiras e do comércio 
-não-essencial, além de restringir o transporte público e toda circulação não 
-estritamente necessária (medidas de restrição) - tomando porém medidas para 
-garantir o abastecimento de alimentos e remédios nas cidades. O uso de máscaras
- em espaços públicos se torna obrigatório. Casos confirmados e suspeitos de 
- Covid-19 são isolados em suas casas.   
-""")
+        <i>**ESTRATÉGIA 2:** **O governo decreta o fechamento das fronteiras e do comércio não-essencial, além de restringir o transporte público e toda circulação não estritamente necessária**(medidas de restrição) - tomando porém medidas para 
+        garantir o abastecimento de alimentos e remédios nas cidades. O uso de máscaras
+        em espaços públicos se torna obrigatório. Casos confirmados e suspeitos de 
+        Covid-19 são isolados em suas casas.</i>
+        """, unsafe_allow_html=True)
 
         st.write("""
-**ESTRATÉGIA 2:** O governo amplia a capacidade de testes e proíbe estritamente
- o movimento das pessoas não-autorizadas (lockdown), com forte monitoramento, 
- mas sem prejuízo ao abastecimetno das cidades de ítens essenciais. Casos 
- confirmados são isolados em hospitais, escolas e hotéis designados. 
- Profissionais de saúde recebem equipamentos de proteção individual adequados 
- e alojamento, para evitar infecção de suas famílias. 
-""")
+        <i>**ESTRATÉGIA 3:** <b>O governo amplia a capacidade de testes e proíbe estritamente
+        o movimento das pessoas não-autorizadas</b>s (lockdown), com forte monitoramento, 
+        mas sem prejuízo ao abastecimetno das cidades de ítens essenciais. Casos 
+        confirmados são isolados em hospitais, escolas e hotéis designados. 
+        Profissionais de saúde recebem equipamentos de proteção individual adequados 
+        e alojamento, para evitar infecção de suas famílias.</i>
+        """, unsafe_allow_html=True)
 
         st.write("""
-A Estratégia 3 exigiu um esforço coordenado entre governo e sociedade: foram 
-parceiros no investimento em expansão de leitos e fornecimento de equipamentos 
-de saúde. Ao reduzir a transmissão e expandir a capacidade de tratamento, a 
-cidade conseguiu "virar a curva."
-""")
+        A Estratégia 3 exigiu um esforço coordenado entre governo e sociedade: foram 
+        parceiros no investimento em expansão de leitos e fornecimento de equipamentos 
+        de saúde. Ao reduzir a transmissão e expandir a capacidade de tratamento, a 
+        cidade conseguiu "virar a curva."
+        """)
 
         st.write("""
 ## Simule o impacto de estratégias semelhantes na capacidade o sistema de saúde em sua cidade:
