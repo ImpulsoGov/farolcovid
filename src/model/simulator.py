706b89c3c4a581637682bb072f3468c65e58eba2
Fernanda Scovino <fscovinom@gmail.com>
2020-04-08 01:59:15 -0300

    update plot design + add capacity constraint line

--- src/model/simulator.py
+++ src/model/simulator.py
@@ -132,14 +132,24 @@ def plot_fig(t, cols):
             fig.add_trace(go.Scatter(x=t.index, y=t[col], name=cols[i_type]['name'], fill='tonexty',
                                      hovertemplate=None, #'%{y:.0f} no dia %{x}',
                                      mode='lines', line=dict(color=cols[i_type]['color'], width=3)))
-
-    fig.update_layout(template='plotly_white', 
-                      legend_orientation="h", legend=dict(x=0, y=-.2, font=dict(size=14)),
-                      hovermode='x')
     
-    fig.update_xaxes(title=t.index.name, titlefont=dict(size=12), showline=True, 
-                     linewidth=2, linecolor='black', showgrid=False)
-    fig.update_yaxes(gridwidth=1, gridcolor='#e6e6e6')
+    for i_type in cols.keys():
+        fig.add_trace(go.Scatter(x=t.index, y=[cols[i_type]['capacity'] for i in t.index], name=cols[i_type]['resource_name'],
+                                showlegend=False, hovertemplate=None, mode='lines',
+                                line=dict(color=cols[i_type]['color'], width=6, dash="dash")))
+
+    fig.update_layout(title="<b>EVOLUÇÃO DIÁRIA DA DEMANDA HOSPITALAR</b>", titlefont=dict(size=30, family='Oswald, sans-serif'), 
+                    paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)',
+                    legend_orientation="h", legend=dict(x=0, y=-.2, font=dict(size=18, family='Oswald, sans-serif')),
+                    hovermode='x', 
+                    autosize=False, width=1000, height=800)
+    
+    fig.update_xaxes(title="dias", tickfont=dict(size=16, family='Oswald, sans-serif'),
+                    # titletext=dict(xref='paper', x=0),
+                    titlefont=dict(size=18, family='Oswald, sans-serif'), 
+                    showline=True, linewidth=2, linecolor='black', showgrid=False)
+
+    fig.update_yaxes(gridwidth=1, gridcolor='#d7d8d9', tickfont=dict(size=16, family='Oswald, sans-serif'),)
     
     return fig
 
@@ -147,15 +157,18 @@ def run_evolution(user_input):
     
     dfs = run_simulation(user_input['population_params'], user_input['strategy'])
     
-    cols = {'I2': {'name': 'Demanda por leitos', 'color': '#f4511e'}, 
-            'I3': {'name': 'Demanda por ventiladores', 'color': '#ff8f00'}}
+    cols = {'I2': {'name': 'Demanda por leitos', 'color': '#F2C94C', 
+                  'resource_name': 'Capacidade de leitos', 'capacity': user_input['n_beds']}, 
+            'I3': {'name': 'Demanda por ventiladores', 'color': '#0097A7',
+                  'resource_name': 'Capacidade de ventiladores', 'capacity': user_input['n_ventilators']}}
 
     # Create graph
     t = dfs['best'][cols.keys()].join(dfs['worst'][cols.keys()], lsuffix='_best', rsuffix='_worst').sort_index(axis=1)    
-    fig = plot_fig(t, cols)
     
     dday_beds = get_dday(dfs,'I2', user_input['n_beds'])   
     dday_ventilators = get_dday(dfs, 'I3', user_input['n_ventilators'])
+    
+    fig = plot_fig(t, cols)
     
     return fig, dday_beds, dday_ventilators
 
