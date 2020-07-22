080f488c43efa4728adf887390ee9033cefc5074
Victor Cortez <victorcortezcb@gmail.com>
2020-07-22 15:36:28 +0000

    Turned off saude em ordem and solved conflicts with stable

--- src/pages/main.py
+++ src/pages/main.py
@@ -17,6 +17,7 @@ import amplitude
 import session
 
 from streamlit.server.Server import Server
+import os
 
 
 def fix_type(x, group):
@@ -182,6 +183,35 @@ def get_data(config):
 
 
 def main(session_state):
+    # START Google Analytics
+    GOOGLE_ANALYTICS_CODE = os.getenv("GOOGLE_ANALYTICS_CODE")
+    if GOOGLE_ANALYTICS_CODE:
+        import pathlib
+        from bs4 import BeautifulSoup
+
+        GA_JS = (
+            """
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '%s');
+        """
+            % GOOGLE_ANALYTICS_CODE
+        )
+        index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+        soup = BeautifulSoup(index_path.read_text(), features="lxml")
+        if not soup.find(id="google-analytics-loader"):
+            script_tag_import = soup.new_tag(
+                "script",
+                src="https://www.googletagmanager.com/gtag/js?id=%s"
+                % GOOGLE_ANALYTICS_CODE,
+            )
+            soup.head.append(script_tag_import)
+            script_tag_loader = soup.new_tag("script", id="google-analytics-loader")
+            script_tag_loader.string = GA_JS
+            soup.head.append(script_tag_loader)
+            index_path.write_text(str(soup))
+    # END Google Analytics
 
     # Get user info
     user_analytics = amplitude.gen_user(utils.get_server_session())
