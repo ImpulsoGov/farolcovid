2f56d1806276c0a755e0edd472033fb16ac333be
Victor Cortez <victorcortezcb@gmail.com>
2020-07-24 20:50:11 +0000

    Replaces the saude vs simula dropdown in favor or making the cards clickable

--- src/utils.py
+++ src/utils.py
@@ -346,6 +346,38 @@ def reload_window():
 
 # END OF AMPLITUDE HELPER METHODS
 
+# JAVASCRIPT HACK METHODS
+
+
+def stylizeButton(name, style_string, session_state, others=dict()):
+    """ adds a css option to a button you made """
+    session_state.button_styles[name] = [style_string, others]
+
+
+def applyButtonStyles(session_state):
+    """ Use it at the end of the program to apply styles to buttons as defined by the function above """
+    time.sleep(0.1)
+    html = ""
+    for name, style in session_state.button_styles.items():
+        parts = (
+            style[0]
+            .replace("\n", "")
+            .replace("    ", "")
+            .replace("; ", "&")
+            .replace(";", "&")
+            .replace(":", "=")
+        )
+        other_args = "&".join(
+            [str(key) + "=" + str(value) for key, value in style[1].items()]
+        )
+        html += f"""
+        <iframe src="resources/redo-button.html?name={name}&{parts}&{other_args}" heigth = 0 width = 0>
+        </iframe>"""
+    st.write(html, unsafe_allow_html=True)
+
+
+# END OF JAVASCRIPT HACK METHODS
+
 
 def gen_pdf_report():
     st.write(
